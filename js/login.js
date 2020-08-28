e8471b083c654da7ce1b471d70efbc1822facb8a
unknown <daikiekobayashi@hotmail.com>
2020-08-28 23:45:09 +0900

    DOM

---
+++ js/login.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", function(event) {
+	var urlParam = location.search.substring(1);
+	if(urlParam) {
+		var param = urlParam.split('=');
+		if(param[0] = "ID") {
+			var ida = [];
+			for(i=0; i<param[1].length; i++) {
+				ida[i] = Number(param[1].substr(i, 1));
+			}
+			var enc = String(ida[1]);
+			var chk = 0;
+			var idb = [];
+			idb[0] = ida[1];
+			for(j=1; j<ida.length-1; j++) {
+				idb[j] = (idb[j-1] + ida[j+1]) % 10;
+				enc = enc.concat(idb[j]);
+				chk = chk + Number(ida[j+1]);
+			}
+		}
+		var req = new XMLHttpRequest();
+		req.open("GET", "https://daikie.github.io/NCPDA/unavailable.txt", true);
+		req.onload = function() {
+			var paths = req.responseText.split("\n");
+			for(ln=0; ln<paths.length; ln++) {
+				if(enc == paths[ln]) {
+					chk = chk + 1;
+					break;
+				}
+			}
+			var htm = "";
+			htm += "<h2>西小山クリニック　処置予約ページ</h2>";
+			htm += "<p>継続的な通院が必要な処置の方のための予約ページです。</p>"
+			if(chk % 10 == ida[0]) {
+				htm += "<p>ご利用中の処置予約ページは<span class='markR'>カルテ番号" + enc + "の方専用</span>です。</p>";
+				htm += "<p>30分に1枠となっております。30日先までの予約が可能です。<p>";
+				htm += "<p>直前キャンセル、遅延、無断キャンセルが計3回以上生じた場合や、<br>予約システムの乱用が見られた場合は処置予約をご利用になれなくなります。</p>";
+				htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s0000234AB', target='_blank'>予約ページに行く</a></h2>";
+				htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+			} else {
+				htm += "<p>現在処置予約を取得する権限がありません。</p>";
+				htm += "<p>原因は以下の可能性があります。</p>";
+				htm += "<ul><li>IDの入力間違い</li><li>処置が終了した</li><li>予約の無断キャンセル、複数予約取得などの規約違反</li></ul>";
+				htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+			}
+			document.getElementById("main-content").innerHTML = htm;
+		}
+		req.send(null);
+	} else {
+		alert("パラメーターがありません。");
+	}
+});
