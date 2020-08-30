bbccea234af3844d51a12fd006c40fd2569e5383
unknown <daikiekobayashi@hotmail.com>
2020-08-30 21:33:39 +0900

    Added certificate HTML

--- js/original.js
+++ js/original.js
@@ -1,9 +1,1 @@
-// Close navbar when clicked outside
-$(function() {
-	$(".navbar-toggler").blur(function(event) {
-		var screenWidth = window.innerWidth;
-		if (screenWidth < 768) {
-			$(".navbar-collapse").collapse('hide');
-		}
-	});
-});
+// Original script
