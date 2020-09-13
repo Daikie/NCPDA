7101d5bdd0fa8b08b9fc6e58832cd05444a94c04
unknown <daikiekobayashi@hotmail.com>
2020-09-14 03:22:41 +0900

    ajax1

--- js/original.js
+++ js/original.js
@@ -1,1 +1,57 @@
-// Original script
+(function (global) {
+// Set up a namespace for our utility
+var ajaxUtils = {};
+// Returns an HTTP request object
+function getRequestObject() {
+	if (global.XMLHttpRequest) {
+		return (new XMLHttpRequest());
+	} else if (global.ActiveXObject) { // For very old IE browsers
+		return (new ActiveXObject("Microsoft.XMLHTTP"));
+	} else {
+		global.alert("Ajax is not supported in your browser.");
+		return(null);
+	}
+}
+
+// Makes an Ajax GET request to 'requestUrl'
+ajaxUtils.sendGetRequest = function(requestUrl, responseHandler) {
+		var request = getRequestObject();
+		request.onreadystatechange =
+			function() {
+				handleResponse(request, responseHandler);
+			};
+		request.open("GET", requestUrl, true);
+		request.send(null);
+	}
+
+// Only calls user provided 'responseHandler' function if response is ready and not an error
+function handleResponse(request, responseHandler) {
+	if ((request.readyState == 4) && (request.status == 200)) {
+		// Default to isJsonResponse = true
+		if (isJsonResponse == undefined) {
+			isJsonResponse = true;
+		}
+		if (isJsonResponse) {
+			responseHandler(JSON.parse(request.responseText));
+		}
+		else {
+			responseHandler(request.responseText);
+		}
+ 	}
+}
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+})(window);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+	document.querySelector(".btn-success").addEventListener("click", function () {
+		// Call server to get the name
+		$ajaxUtils.sendGetRequest("snippet.txt", function (request) {
+			var name = request.responseText;
+			console.log(name);
+			document.querySelector("#content").innerHTML = name;
+			});
+		});
+	});
+
