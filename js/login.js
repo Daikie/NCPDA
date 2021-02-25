79d61d7ac5060c98ca86195cd800fdc269566cb4
unknown <daikiekobayashi@hotmail.com>
2021-02-25 23:54:31 +0900

    OnetimePassword

--- js/login.js
+++ js/login.js
@@ -27,23 +27,43 @@ window.addEventListener("load", function(event) {
 					break;
 				}
 			}
-			var htm = "";
-			htm += "<h2>西小山クリニック　処置予約ページ</h2>";
-			htm += "<p>継続的な通院が必要な処置の方のための予約ページです。</p>"
-			if(chk % 10 == ida[0]) {
-				htm += "<p>ご利用中の処置予約ページは<span class='markR'>カルテ番号" + enc + "の方専用</span>です。</p>";
-				htm += "<p>30分に1枠となっております。30日先までの予約が可能です。<p>";
-				htm += "<p>直前キャンセル、遅延、無断キャンセルが計3回以上生じた場合や、<br>予約システムの乱用が見られた場合は処置予約をご利用になれなくなります。</p>";
-				htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s0000234AB', target='_blank'>予約ページに行く</a></h2>";
-				htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+			if(enc.length == 8) {
+				var today = new Date();
+				var encdate = new Date(Number(enc.substr(0,4)), Number(enc.substr(4,2))-1, Number(enc.substr(6,2)));
+				encdate.setDate(encdate.getDate()+8);
+				var htm = "";
+				htm += "<h2>西小山クリニック　ワンタイム予約ページ</h2>";
+				htm += "<p>近日中の受診を希望される方のための予約ページです。</p>";
+				htm += "<p>このページは<font color=#00f>"+encdate.getFullYear()+"年"+(encdate.getMonth()+1)+"月"+encdate.getDate()+"日まで</font>使用できます。</p>";
+				encdate.setDate(encdate.getDate()+1);
+				if(chk % 10 == ida[0] && today<=encdate) {
+					htm += "<p>ご希望受診日の<font color=#f00>3日前19時から前日19時まで</font>予約が可能です。</p>";
+					htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s00004880D', target='_blank'>予約ページに行く</a></h2>";
+					htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+				} else {
+					htm += "<p>受付期間外です。</p>";
+					htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+				}
+				document.getElementById("main-content").innerHTML = htm;
 			} else {
-				htm += "<p>現在処置予約を取得する権限がありません。</p>";
-				htm += "<p>原因は以下の可能性があります。</p>";
-				htm += "<ul><li>IDの入力間違い</li><li>処置が終了した</li><li>予約の無断キャンセル、複数予約取得などの規約違反</li></ul>";
-				htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+				var htm = "";
+				htm += "<h2>西小山クリニック　処置予約ページ</h2>";
+				htm += "<p>継続的な通院が必要な処置の方のための予約ページです。</p>"
+				if(chk % 10 == ida[0]) {
+					htm += "<p>ご利用中の処置予約ページは<span class='markR'>カルテ番号" + enc + "の方専用</span>です。</p>";
+					htm += "<p>30分に1枠となっております。30日先までの予約が可能です。<p>";
+					htm += "<p>直前キャンセル、遅延、無断キャンセルが計3回以上生じた場合や、<br>予約システムの乱用が見られた場合は処置予約をご利用になれなくなります。</p>";
+					htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s0000234AB', target='_blank'>予約ページに行く</a></h2>";
+					htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+				} else {
+					htm += "<p>現在処置予約を取得する権限がありません。</p>";
+					htm += "<p>原因は以下の可能性があります。</p>";
+					htm += "<ul><li>IDの入力間違い</li><li>処置が終了した</li><li>予約の無断キャンセル、複数予約取得などの規約違反</li></ul>";
+					htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+				}
+				document.getElementById("main-content").innerHTML = htm;
 			}
-			document.getElementById("main-content").innerHTML = htm;
-		}
+		}	
 		req.send(null);
 	} else {
 		alert("パラメーターがありません。");
