a020962712807fe9288acef0b62244e46b75811a
Daikie <daikiekobayashi@hotmail.com>
2022-08-16 05:03:25 +0900

    Novel Advance

--- js/refill_login.js
+++ js/refill_login.js
@@ -22,24 +22,44 @@ window.addEventListener("load", function(event) {
 		var duedate = new Date(Number(enc.substr(0,4)), Number(enc.substr(4,2))-1, Number(enc.substr(6,2)));
 		frmdate.setDate(frmdate.getDate());
 		duedate.setMonth(duedate.getMonth()+2);
-
+		if (enc.length == 8) {	// Old Advance
+			var grade = 0;
+		} else {				// Novel Advance
+			var grade = enc.substr(8,1);
+		}
+		
 		var req = new XMLHttpRequest();
 		req.open("GET", "https://daikie.github.io/NCPDA/unavailable.txt", true);
 		req.onload = function() {
 			var htm = "";
 			htm += "<h2>西小山クリニック　アドバンス再診予約</h2>";
 			htm += "<p>数日前から予約を取得されたい方のための予約ページです。</p>";
-			htm += "<p>このページは<font color=#00f>"+frmdate.getFullYear()+"年"+(frmdate.getMonth()+1)+"月"+frmdate.getDate()+"日から";
-			htm += duedate.getFullYear()+"年"+(duedate.getMonth()+1)+"月"+duedate.getDate()+"日まで</font>使用できます。</p>";
-			duedate.setDate(duedate.getDate()+1);
-			// if(chk % 10 == ida[0] && today>=frmdate && today<=duedate) {
-			if(chk % 10 == ida[0] && today<=duedate) {
-				htm += "<p>ご希望受診日の<font color=#f00>3日前19時から前日19時まで</font>予約が可能です。</p>";
-				htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s00004880D', target='_blank'>予約ページに行く</a></h2>";
-				htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+			if(chk % 10 == ida[0]) {
+				htm += "<p>この予約ページの有効期間：<font color=#00f><br>"
+				// htm += frmdate.getFullYear()+"年"+(frmdate.getMonth()+1)+"月"+frmdate.getDate()+"日から<br>";
+				htm += duedate.getFullYear()+"年"+(duedate.getMonth()+1)+"月"+duedate.getDate()+"日まで</font></p>";
+				duedate.setDate(duedate.getDate()+1);
+				if(today <= duedate) {
+					htm += "<p>予約受付期間：<br>"
+					htm += "ご希望受診日の<font color=#f00>3日前19時から</font><br>";
+					htm += "ご希望受診日の<font color=#f00>前日19時まで</font>";
+					if(grade == 0) {
+						htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s00004880D', target='_blank'>予約ページに行く</a></h2>";
+					} else if(grade == 1) {
+						htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s0000752D1', target='_blank'>予約ページに行く</a></h2>";
+					} else if(grade == 2) {
+						htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s0000752F0', target='_blank'>予約ページに行く</a></h2>";
+					}
+					htm += "<p>※予約が埋まり次第受付終了となります。</p>"
+					htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+				} else {
+					htm += "<p>受付期間外です。";
+					htm += "<br>期間内にも関わらず予約ページが表示されない場合は更新ボタンを押してください。</p>";
+					htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
+				}
 			} else {
-				htm += "<p>受付期間外です。";
-				htm += "<br>期間内にも関わらず予約ページが表示されない場合は更新ボタンを押してください。</p>";
+				htm += "<p>パラメーターの指定が間違っています。";
+				htm += "<br>QRコードの再読み込みをお願いします。</p>";
 				htm += "<br><h2 id='back'><a href='index.html'>戻る</a></h2>";
 			}
 			document.getElementById("main-content").innerHTML = htm;
