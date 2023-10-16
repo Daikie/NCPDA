060c650af63f127edcf317ee6e6f10326aee694d
Daikie <daikiekobayashi@hotmail.com>
2023-10-16 22:48:24 +0900

    reserveBtn

--- js/verify.js
+++ js/verify.js
@@ -50,12 +50,13 @@
 			for(let j = 0; j < csvArray.length; j++) {
 				if(hash == csvArray[j][0]) {
 					durt = Number.parseFloat(csvArray[j][3]) + Number.parseFloat(csvArray[j][4]);
+					icon = '<svg viewBox="0 0 16 16" class="bi bi-calendar-check" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path fill-rule="evenodd" d="M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1zm1-3a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"/><path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"/></svg>'
 					if(durt < 540) {
-						msg.innerHTML = '<p>カルテ番号：' + csvArray[j][1] + '</p><a href="https://airrsv.net/nishicli/calendar?schdlId=s00008E234">短い</a>';
-						window.open("https://airrsv.net/nishicli/calendar?schdlId=s00008E234", "_blank");
+						msg.innerHTML = '<p>カルテ番号：' + csvArray[j][1] + '</p><h2 id="reserve"><a href="https://airrsv.net/nishicli/calendar?schdlId=s00008E234">' + icon + '短い</a></h2>';
+						
 					} else {
-						msg.innerHTML = '<p>カルテ番号：' + csvArray[j][1] + '</p><a href="https://airrsv.net/nishicli/calendar?schdlId=s00008E234">長い</a>';
-						window.open("https://airrsv.net/nishicli/calendar?schdlId=s00008E234", "_blank");
+						msg.innerHTML = '<p>カルテ番号：' + csvArray[j][1] + '</p><h2 id="reserve"><a href="https://airrsv.net/nishicli/calendar?schdlId=s00008E234">' + icon + '長い</a></h2>';
+						
 					}
 					
 				} else {
