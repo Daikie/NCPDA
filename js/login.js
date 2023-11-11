1b13a0b260958bfe7a5ecfedb47a5f96d0179fcd
Daikie <daikiekobayashi@hotmail.com>
2023-11-11 20:30:52 +0900

    id2023/11/11-20:30

--- js/login.js
+++ js/login.js
@@ -72,10 +72,12 @@ window.addEventListener("load", function(event) {
 								break;
 							}
 						}
+						const suite = [3598, 23253, 25414, 27623];
+						console.log(suite.includes(enc));
 						htm += "<p>ご利用中の処置予約ページは<span class='markR'>カルテ番号" + enc + "の方専用</span>です。</p>";
 						htm += "<p>30日後までの予約が可能です。</p>";
 						htm += "<p>直前キャンセル、遅延、無断キャンセルが計3回以上生じた場合や、<br>予約システムの乱用が見られた場合は処置予約をご利用になれなくなります。</p>";
-						if(skip < 3 && durt < 560) {
+						if(skip < 3 && durt < 560 || suite.includes(enc)) {
 							htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s0000234AB', target='_blank'>予約ページに行く</a></h2>";
 						} else {
 							htm += "<h2 id='reserve'><a href='https://airrsv.net/nishicli/calendar?schdlId=s00009401B', target='_blank'>予約ページに行く</a></h2>";
