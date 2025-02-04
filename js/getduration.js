5a9ba250109c4600864468d2e61a129598f8c001
Daikie <daikiekobayashi@hotmail.com>
2025-02-05 00:25:22 +0900

    limit

--- js/getduration.js
+++ js/getduration.js
@@ -17,11 +17,8 @@ window.addEventListener("load", function(event) {
 		var am;
 		var pm;
 		var today = new Date();
-		var timeH = today.getHours();
-		var timeM = today.getMinutes();
+		var t = today.getHours() * 60 + today.getMinutes();
 		var rest;
-		console.log(timeH);
-		console.log(timeM);
 		for(let j = 0; j < csvArray.length; j++) {
 			if(csvArray[j][1] == 1) {
 				avgDur = csvArray[j][3];
@@ -35,35 +32,34 @@ window.addEventListener("load", function(event) {
 		document.getElementById("duration").innerHTML += min + "分" + sec + "秒";
 		document.getElementById("am").innerHTML += am + "人";
 		document.getElementById("pm").innerHTML += pm + "人";
-		console.log(document.getElementById("count-person").textContent);
 		var countPerson = document.getElementById("count-person").textContent;
 		var l = countPerson.replace(/[^0-9]/g, "");
 		console.log(l);
-		if(timeH < 9) {
+		if(t < 570) {			// 9:30
 			rest = "--";
-		} else if(timeH == 9) {
-			if(timeM < 30) {
-				rest = "--";
-			} else {
-				rest = Math.floor(((12 - timeH) * 60 + 30 - timeM) * 60 / avgDur) - l;
-			}
-		} else if(timeH < 13) {
-			rest = Math.floor(((12 - timeH) * 60 + 30 - timeM) * 60 / avgDur) - l;
-		} else if (timeH < 15) {
+		} else if(t < 750) {	// 12:30
+			rest = Math.floor((750 - t) * 60 / avgDur) - l;
+		} else if(t < 930) {	// 15:30
 			rest = "--";
-		} else if (timeH == 15) {
-			if(timeM < 30) {
-				rest = "--";
-			} else {
-				rest = Math.floor(((18 - timeH) * 60 + 30 - timeM) * 60 / avgDur) - l;
-			}
-		} else if (timeH < 19) {
-			rest = Math.floor(((18 - timeH) * 60 + 30 - timeM) * 60 / avgDur) - l;
+		} else if (t < 1110) {	// 18:30
+			rest = Math.floor((1110 - t) * 60 / avgDur) - l;
 		} else {
 			rest = "--";
 		}
 		console.log(rest);
-		document.getElementById("count-rest").innerHTML = "残り" + rest + "人";
+		if (rest == "--") {
+			document.getElementById("count-rest").innerHTML = "--";
+		} else if (rest <= 0) {
+			document.getElementById("count-rest").innerHTML = "残り0人";
+		} else {
+			document.getElementById("count-rest").innerHTML = "残り" + rest + "人";
+			queue = document.getElementById("queue");
+			queue.getElementsByTagName("a")[0].setAttribute("href", "https://airwait.jp/WCSP/storeDetail?storeNo=AKR9456100837");
+			queue.getElementsByTagName("a")[0].setAttribute("class", "active");
+			queue.innerHTML = queue.innerHTML.replace("受付停止", "を取得する");
+		}
+		
+		
 	}
 	req.send(null);
 });
