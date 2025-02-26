29eb62ada0033f0e7eb16d9d9eff00b6183eaf71
Daikie <daikiekobayashi@hotmail.com>
2025-02-26 13:58:45 +0900

    id2025/02/26-13:58

--- js/delayed_action.js
+++ js/delayed_action.js
@@ -13,3 +13,14 @@ if (today < switchDate) {
 	console.log("Showing sideB");
 }
 elmSD.remove();
+
+const dat = {
+        'url' : 'https://airwait.jp/WCSP/api/20160600/external/stateless/store/getWaitInfo?key=Gh821KMxF8MMPmmQUIpycVeNRAXVEd99&storeId=KR00378855',
+        'type' : 'GET',
+    }
+const req = new XMLHttpRequest();
+req.open("GET", "https://airwait.jp/WCSP/api/20160600/external/stateless/store/getWaitInfo?key=Gh821KMxF8MMPmmQUIpycVeNRAXVEd99&storeId=KR00378855", true);
+req.addEventListener("load", function() {
+    const res = req.responseText;
+    console.log(res);
+}
