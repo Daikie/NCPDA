9a82d63958d8ad87145fdc264597ed60f8c70c7c
Daikie <daikiekobayashi@hotmail.com>
2025-02-26 23:39:29 +0900

    id2025/02/26-23:39

--- js/delayed_action.js
+++ js/delayed_action.js
@@ -13,11 +13,3 @@ if (today < switchDate) {
 	console.log("Showing sideB");
 }
 elmSD.remove();
-
-const req = new XMLHttpRequest();
-req.open("POST", "https://cl.airwait.jp/WCLP/api/external/stateless/reservations?key=Gh821KMxF8MMPmmQUIpycVeNRAXVEd99&storeId=KR00378855&waitTypeId=0001", true);
-req.addEventListener("load", function() {
-    const res = req.responseText;
-    console.log(res);
-});
-req.send(null);
